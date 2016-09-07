6685c9a47b7abd7aaef0ba6a46674e26e74305d7
Wilder Gonzales <wilder@mailtrack.io>
2016-09-07 15:58:20 +0200

    12: basic routing

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ var app = express();
 app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
-	res.render("index", {title: "Hello World!"});
+	res.render("index");
+});
+
+app.get("/:value", function (req, res) {
+	res.render("form", {name: req.params.value});
+});
+
+app.post("/", function (req, res) {
+	res.render("form");
 });
 
 app.listen(8080);
-
